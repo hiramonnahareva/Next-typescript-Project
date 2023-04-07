86bf5ae3fad71b6549fc6e77071b2342ce769a19
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-04-08 01:06:42 +0600

    add navbar

---
+++ src/components/ui/LargeHeading.tsx
@@ -0,0 +1,36 @@
+import { FC, forwardRef,HTMLAttributes, HtmlHTMLAttributes } from "react";
+import { VariantProps, cva } from 'class-variance-authority'
+import { cn } from "@/lib/utils";
+
+const headingVariants = cva(
+    'texxt-black dark:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter',
+    {
+        variants: {
+            size: {
+                default: 'text-4xl md:text-5xl lg:text-6xl',
+                lg: 'text-5xl md:text-6xl lg:text-7xl',
+                sm: 'text-2xl, lg:text-4xl',
+            },
+        },
+        defaultVariants: {
+            size: 'default',
+        },
+    }
+
+)
+
+interface LargeHeadingProps
+ extends HTMLAttributes<HTMLHeadingElement>,
+ VariantProps<typeof headingVariants> {}
+const Paragraph = forwardRef<HTMLParagraphElement, LargeHeadingProps>(({
+    className, size, children, ...props
+}, ref) => {
+    return <h1
+    ref={ref} 
+    {...props} 
+    className={cn(headingVariants({size, className}))}>
+        {children}
+    </h1>
+})
+Paragraph.displayName ='Paragraph'
+export default Paragraph;
---
+++ src/components/ui/SigninButton.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { FC, useState } from 'react'
+import Button from './Button'
+import { signIn } from 'next-auth/react'
+
+interface SigninButtonProps {
+  
+}
+
+const SigninButton: FC<SigninButtonProps> = ({}) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const signInwithGoogle = async() => {
+        setIsLoading(true)
+        try {
+            await signIn('google')
+        }
+        catch (error) {
+            // toast({
+            //     title: 'Error sign in',
+            //     message: 'Please Try again later',
+            //     type: 'error',
+            // })
+        }
+    }
+  return <Button onClick={signInwithGoogle} isLoading={isLoading}>
+    Sign in</Button>
+}
+
+export default SigninButton
